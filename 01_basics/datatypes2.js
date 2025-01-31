81da4e0b65ce252abdd7dab5ed0d90e506249312
khushwanth3218 <justtolearn123@gmail.com>
2025-01-31 07:18:47 +0000

    var comitted

---
+++ 01_basics/datatypes2.js
@@ -0,0 +1,11 @@
+let a="KHUSH" //STRIGNG
+let b= 'hgy' //string
+let c= null
+let d;
+let f =true;
+console.table(typeof (a) )
+console.table(typeof (b) )
+console.table(typeof (c) )
+console.table(typeof (d) )
+console.table(typeof (f) )
+console.table(typeof (undefined) )
---
+++ 01_basics/variables1.js
@@ -0,0 +1,5 @@
+const a="kshuh";
+let b="jfz"
+ b="jsfj"
+console.table([a,b])
+//dot use var ans direct initializaion like a=10 use let a=10
