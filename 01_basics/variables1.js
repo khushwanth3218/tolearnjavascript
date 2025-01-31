8fb91ddd75ab8d0478359e2749e1aa6c1d32b3b1
khushwanth3218 <justtolearn123@gmail.com>
2025-01-31 07:21:38 +0000

    var and datatypes commited

--- 01_basics/variables1.js
+++ 01_basics/variables1.js
@@ -2,4 +2,4 @@ const a="kshuh";
 let b="jfz"
  b="jsfj"
 console.table([a,b])
-//dot use var ans direct initializaion like a=10 use let a=10
+//dot use var ans direct initializaion like a=10 use let a=10 
